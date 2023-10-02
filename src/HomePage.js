80608dffefcc16262e8231edca3e2b4fbf607e2a
James Staud <3604900+Jstaud@users.noreply.github.com>
2023-10-02 15:16:10 -0500

    reformatting the homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,50 +7,77 @@ function HomePage() {
   return (
     <div>
       {/* Header */}
-      <header>
+      <header className="bg-dark text-white py-5">
         <Container>
           <Row>
-            <Col>
-              <h1>Welcome to ProtestPal</h1>
-              <p>Your trusted partner in property tax protests.</p>
+            <Col lg={8}>
+              <h1 className="display-4">Welcome to ProtestPal</h1>
+              <p className="lead">
+                Your trusted partner in property tax protests.
+              </p>
               <Button variant="primary" size="lg">
                 Get Started
               </Button>
-            </Col>
-            <Col>
-              {/* You can add an image or logo here */}
             </Col>
           </Row>
         </Container>
       </header>
 
       {/* Hero Section */}
-      <Card className="bg-info text-white">
+      <section className="bg-light py-5">
         <Container>
-          <Card.Body>
-            <h2>Empower Yourself with ProtestPal</h2>
-            <p>
-              Take control of your property taxes. File protests, connect with
-              others, and stay informed about tax-related news.
-            </p>
-            <Button variant="light">Learn More</Button>
-          </Card.Body>
+          <Row>
+            <Col lg={6}>
+              <h2 className="display-5">Empower Yourself with ProtestPal</h2>
+              <p>
+                Take control of your property taxes. File protests, connect with
+                others, and stay informed about tax-related news.
+              </p>
+              <Button variant="info">Learn More</Button>
+            </Col>
+            <Col lg={6}>
+              <Card>
+                <Card.Body>
+                  <h5>Sign Up for Free</h5>
+                  <p>
+                    Join ProtestPal and start saving on your property taxes
+                    today.
+                  </p>
+                  <Button variant="primary">Sign Up</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Container>
-      </Card>
+      </section>
 
-      {/* Other Content */}
-      <Container>
-        <Row>
-          <Col>
-            <h3>Features</h3>
-            {/* Add feature descriptions here */}
-          </Col>
-          <Col>
-            <h3>Testimonials</h3>
-            {/* Add testimonials or user reviews here */}
-          </Col>
-        </Row>
-      </Container>
+      {/* Featured Section */}
+      <section className="py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h2 className="display-5">Featured Services</h2>
+            </Col>
+          </Row>
+          <Row>
+            {/* Add featured service cards here */}
+          </Row>
+        </Container>
+      </section>
+
+      {/* Testimonials Section */}
+      <section className="bg-light py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h2 className="display-5">What Our Users Say</h2>
+            </Col>
+          </Row>
+          <Row>
+            {/* Add user testimonial cards here */}
+          </Row>
+        </Container>
+      </section>
     </div>
   );
 }
